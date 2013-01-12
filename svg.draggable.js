27d9dcbd9e7ed942cc62d75ea22caeb26bc94c2e
wout <wout@impinc.co.uk>
2013-01-12 18:13:35 +0100

    Implemented viewBox awareness

--- svg.draggable.js
+++ svg.draggable.js
@@ -1,9 +1,11 @@
-// svg.draggable.js 0.1 - Copyright (c) 2013 Wout Fierens - Licensed under the MIT license
+// svg.draggable.js 0.2 - Copyright (c) 2013 Wout Fierens - Licensed under the MIT license
 
 SVG.extend(SVG.Element, {
   // Make element draggable
   draggable: function() {
-    var drag, end, element = this;
+    var drag, end,
+        element = this,
+        parent  = this._parent(SVG.Nested) || this._parent(SVG.Doc);
     
     /* while dragging */
     drag = function(event) {
@@ -15,7 +17,10 @@ SVG.extend(SVG.Element, {
         };
 
         /* move the element to the right position */
-        element.move(element.startPosition.x + delta.x, element.startPosition.y + delta.y);
+        element.move(
+          element.startPosition.x + delta.x / element.startPosition.zoom,
+          element.startPosition.y + delta.y / element.startPosition.zoom
+        );
 
         /* invoke any callbacks */
         if (element.dragmove)
@@ -40,16 +45,23 @@ SVG.extend(SVG.Element, {
     
     /* bind mousedown event */
     this.on('mousedown', function(event) {
-      var box = element.bbox();
+      var box     = element.bbox(),
+          viewBox = parent.attr('viewBox'),
+          width   = parent.attr('width');
       
       /* store event */
       element.startEvent = event || window.event;
       
       /* store start position */
       element.startPosition = {
-        x: box.x,
-        y: box.y
+        x:    box.x,
+        y:    box.y,
+        zoom: 1
       };
+      
+      /* calculate zoom according to viewbox */
+      if (viewBox != null && width != null)
+        element.startPosition.zoom = parseFloat(width) / parseFloat(viewBox.split(' ')[2])
       
       /* add while and end events to window */
       SVG.on(window, 'mousemove', drag);
