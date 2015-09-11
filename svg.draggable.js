bc015a2cb09a3b840a4f137142021668905319c0
nolazybits <nolazybits@gmail.com>
2015-09-11 16:59:13 +1200

    - touch event for svgjs 1.1.0

--- svg.draggable.js
+++ svg.draggable.js
@@ -1,5 +1,7 @@
-// svg.draggable.js 0.1.0 - Copyright (c) 2014 Wout Fierens - Licensed under the MIT license
+// svg.draggable.js 0.1.1 - Copyright (c) 2014 Wout Fierens - Licensed under the MIT license
 // extended by Florian Loch
+// extended by nolazvbits aka Xavier Martin - http://nolazybits.com
+
 ;(function() {
 
   SVG.extend(SVG.Element, {
@@ -7,31 +9,43 @@
     // Constraint might be a object (as described in readme.md) or a function in the form "function (x, y)" that gets called before every move.
     // The function can return a boolean or a object of the form {x, y}, to which the element will be moved. "False" skips moving, true moves to raw x, y.
     draggable: function(constraint) {
-      var start, drag, end
+      var start, drag, end, startEvent, dragEvent, endEvent
         , element = this
         , parent  = this.parent._parent(SVG.Nested) || this._parent(SVG.Doc)
-      
+        , isTouch = 'ontouchstart' in window || (navigator.msMaxTouchPoints && !navigator.msPointerEnabled);
+
       /* remove draggable if already present */
       if (typeof this.fixed === 'function')
-        this.fixed()
-      
+        this.fixed();
+
+      /* decide on event names based on touch support */
+      if (isTouch) {
+        startEvent = 'touchstart';
+        dragEvent = 'touchmove';
+        endEvent = 'touchend';
+      } else {
+        startEvent = 'mousedown';
+        dragEvent = 'mousemove';
+        endEvent = 'mouseup';
+      }
+
       /* ensure constraint object */
-      constraint = constraint || {}
+      constraint = constraint || {};
       
       /* start dragging */
       start = function(event) {
-        event = event || window.event
+        event = event || window.event;
         
         /* invoke any callbacks */
         if (element.beforedrag)
-          element.beforedrag(event)
+          element.beforedrag(event);
         
         /* get element bounding box */
-        var box = element.bbox()
+        var box = element.bbox();
         
         if (element instanceof SVG.G) {
-          box.x = element.x()
-          box.y = element.y()
+          box.x = element.x();
+          box.y = element.y();
           
         } else if (element instanceof SVG.Nested) {
           box = {
@@ -43,7 +57,7 @@
         }
         
         /* store event */
-        element.startEvent = event
+        element.startEvent = event;
         
         /* store start position */
         element.startPosition = {
@@ -53,11 +67,13 @@
         , height:   box.height
         , zoom:     parent.viewbox().zoom
         , rotation: element.transform('rotation') * Math.PI / 180
-        }
+        , pageX:    event.changedTouches ? event.changedTouches[0].pageX : event.pageX
+        , pageY:    event.changedTouches ? event.changedTouches[0].pageY : event.pageY
+        };
         
         /* add while and end events to window */
-        SVG.on(window, 'mousemove', drag)
-        SVG.on(window, 'mouseup',   end)
+        SVG.on(window, dragEvent, drag);
+        SVG.on(window, endEvent,   end);
         
         /* invoke any callbacks */
         if (element.dragstart)
@@ -70,18 +86,21 @@
       /* while dragging */
       drag = function(event) {
         event = event || window.event
-        
-        if (element.startEvent) {
+
+        if (element.startPosition) {
           /* calculate move position */
           var x, y
             , rotation  = element.startPosition.rotation
             , width     = element.startPosition.width
             , height    = element.startPosition.height
+            , pageX     = element.startPosition.pageX
+            , pageY     = element.startPosition.pageY
+            , position  = event.changedTouches ? event.changedTouches[0] : event
             , delta     = {
-                x:    event.pageX - element.startEvent.pageX,
-                y:    event.pageY - element.startEvent.pageY,
+                x:    position.pageX - element.startPosition.pageX,
+                y:    position.pageY - element.startPosition.pageY,
                 zoom: element.startPosition.zoom
-              }
+              };
           
           /* caculate new position [with rotation correction] */
           x = element.startPosition.x + (delta.x * Math.cos(rotation) + delta.y * Math.sin(rotation))  / element.startPosition.zoom
@@ -89,7 +108,7 @@
           
           /* move the element to its new position, if possible by constraint */
           if (typeof constraint === 'function') {
-            var coord = constraint(x, y)
+            var coord = constraint(x, y);
 
             if (typeof coord === 'object') {
               if (typeof coord.x != 'boolean' || coord.x)
@@ -125,21 +144,22 @@
       /* when dragging ends */
       end = function(event) {
         event = event || window.event
-        
+
         /* calculate move position */
-        var delta = {
-          x:    event.pageX - element.startEvent.pageX
-        , y:    event.pageY - element.startEvent.pageY
-        , zoom: element.startPosition.zoom
-        }
+        var position = event.changedTouches ? event.changedTouches[0] : event
+            , delta    = {
+              x:    position.pageX - element.startPosition.pageX,
+              y:    position.pageY - element.startPosition.pageY,
+              zoom: element.startPosition.zoom
+            }
         
         /* reset store */
-        element.startEvent    = null
-        element.startPosition = null
+        element.startEvent    = null;
+        element.startPosition = null;
 
         /* remove while and end events to window */
-        SVG.off(window, 'mousemove', drag)
-        SVG.off(window, 'mouseup',   end)
+        SVG.off(window, dragEvent, drag);
+        SVG.off(window, endEvent,   end);
 
         /* invoke any callbacks */
         if (element.dragend)
@@ -147,16 +167,16 @@
       }
       
       /* bind mousedown event */
-      element.on('mousedown', start)
+      element.on(startEvent, start);
       
       /* disable draggable */
       element.fixed = function() {
-        element.off('mousedown', start)
+        element.off('mousedown', start);
         
-        SVG.off(window, 'mousemove', drag)
-        SVG.off(window, 'mouseup',   end)
+        SVG.off(window, dragEvent, drag);
+        SVG.off(window, endEvent,   end);
         
-        start = drag = end = null
+        start = drag = end = null;
         
         return element
       }
